307d71a9d94a5306353221be658bfad13026d37f
Gabriel Brun <lydstyl@gmail.com>
2020-03-27 20:22:05 +0100

    first

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Real Estate Documents Creator</h1>
+    </div>
+  );
+}
+
+export default App;
