853d71adb52a8fd6d3f6a32e5cec25aa6c1b95c5
Gabriel Brun <lydstyl@gmail.com>
2020-03-28 14:21:42 +0100

    Dokimo responsive navbar ok

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,27 +5,31 @@ export const Nav = () => {
     <>
       <nav>
         <div className='nav-wrapper'>
-          <a href='#!' className='brand-logo'>
-            Dokimo
-          </a>
-          <a href='#' data-target='mobile-demo' className='sidenav-trigger'>
+          <Link className='brand-logo' to='/'>
+            <i className='material-icons'>business</i> DOKIMO
+          </Link>
+          <Link data-target='mobile-demo' className='sidenav-trigger' to='#'>
             <i className='material-icons'>menu</i>
-          </a>
+          </Link>
           <ul className='right hide-on-med-and-down'>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
             <li>
               <Link to='/lessors'>Bailleurs</Link>
             </li>
-            {/* <li>
-              <i className='material-icons'>face</i>
+            <li>
+              <Link to='/'>Lots</Link>
             </li>
-            <li>Lots</li>
-            <li>Avis d'échéances</li>
-            <li>Quittances</li>
-            <li>Reçus partiel de loyer</li>
-            <li>Révisions de loyer</li> */}
+            <li>
+              <Link to='/'>Avis d'échéance</Link>
+            </li>
+            <li>
+              <Link to='/'>Quittances</Link>
+            </li>
+            <li>
+              <Link to='/'>Reçu partiel de loyer</Link>
+            </li>
+            <li>
+              <Link to='/'>Révisions de loyer</Link>
+            </li>
           </ul>
         </div>
       </nav>
