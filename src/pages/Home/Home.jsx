17be67e699fe4a3bf213db5c27571039a54abb93
Gabriel Brun <lydstyl@gmail.com>
2020-03-30 21:07:22 +0200

    Home redirect to login like a PrivateRoute

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useStoreState } from 'easy-peasy';
+import { useHistory } from 'react-router-dom';
 
 export const Home = () => {
+  const history = useHistory();
+
+  const { email } = useStoreState(state => state.user);
+
+  useEffect(() => {
+    if (!email) {
+      history.push('/login');
+    }
+  }, [history, email]);
+
   return (
     <div className='row'>
       <div className='col s12'>
         <h1>Home</h1>
+
+        {email && <p>Bonjour {email}</p>}
       </div>
     </div>
   );
