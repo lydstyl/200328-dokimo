92bddcc0c1ef55d78bce0e00469061f600a3bafd
Gabriel Brun <lydstyl@gmail.com>
2020-04-07 00:16:18 +0200

    PaymentReceipt in progress

--- src/pages/BatchDetail/BatchDetail.jsx
+++ src/pages/BatchDetail/BatchDetail.jsx
@@ -1,61 +1,61 @@
-import React, { useState } from 'react';
-import { useStoreState } from 'easy-peasy';
+import React from 'react';
+// import { useStoreState } from 'easy-peasy';
 
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 
 export const BatchDetail = () => {
-  const { id } = useParams();
+  // const { id } = useParams();
 
-  const { lessors } = useStoreState((state) => state.lessor);
-  const { tenants } = useStoreState((state) => state.tenant);
-  const { batches, terms } = useStoreState((state) => state.batch);
+  // const { lessors } = useStoreState((state) => state.lessor);
+  // const { tenants } = useStoreState((state) => state.tenant);
+  // const { batches, terms } = useStoreState((state) => state.batch);
 
-  const batch = batches.filter((batch) => batch.id === id)[0];
-  const {
-    lid,
-    tid,
-    charge,
-    balance, // must be editable
-    rent,
-    // beginDate, paymentDeadline
-  } = batch;
+  // const batch = batches.filter((batch) => batch.id === id)[0];
+  // const {
+  //   lid,
+  //   tid,
+  //   charge,
+  //   balance, // must be editable
+  //   rent,
+  //   // beginDate, paymentDeadline
+  // } = batch;
 
-  const lessor = lessors.filter((lessor) => lessor.id === lid)[0];
-  const tenant = tenants.filter((tenant) => tenant.id === tid)[0];
+  // const lessor = lessors.filter((lessor) => lessor.id === lid)[0];
+  // const tenant = tenants.filter((tenant) => tenant.id === tid)[0];
 
-  const {
-    companyName,
-    managerFirstName,
-    managerLastName,
-    address1,
-    address2,
-    postalCode,
-    townName,
-  } = lessor;
+  // const {
+  //   // companyName,
+  //   // managerFirstName,
+  //   // managerLastName,
+  //   // address1,
+  //   // address2,
+  //   // postalCode,
+  //   // townName,
+  // } = lessor;
 
-  const { civility, firstName, lastName } = tenant;
+  // const { civility, firstName, lastName } = tenant;
 
-  const docPlace = 'Raismes';
+  // const docPlace = 'Raismes';
 
-  const [dates, setDates] = useState({
-    docDate: terms[0].rentReceiptDate.stringFr,
-    termFrom: terms[0].termFrom.stringFr,
-    termTo: terms[0].termTo.stringFr,
-  });
-  const [csBalance, setCsBalance] = useState(balance); // component state balance
+  // const [dates, setDates] = useState({
+  //   docDate: terms[0].rentReceiptDate.stringFr,
+  //   termFrom: terms[0].termFrom.stringFr,
+  //   termTo: terms[0].termTo.stringFr,
+  // });
+  // const [csBalance, setCsBalance] = useState(balance); // component state balance
 
-  const onCsBalanceChange = (e) => {
-    setCsBalance(parseFloat(e.target.value));
-  };
+  // const onCsBalanceChange = (e) => {
+  //   setCsBalance(parseFloat(e.target.value));
+  // };
 
-  const handleTermChange = (e) => {
-    const value = JSON.parse(e.target.value);
+  // const handleTermChange = (e) => {
+  //   const value = JSON.parse(e.target.value);
 
-    setDates({
-      docDate: value.rentReceiptDate.stringFr,
-      termFrom: value.termFrom.stringFr,
-      termTo: value.termTo.stringFr,
-    });
-  };
+  //   setDates({
+  //     docDate: value.rentReceiptDate.stringFr,
+  //     termFrom: value.termFrom.stringFr,
+  //     termTo: value.termTo.stringFr,
+  //   });
+  // };
   return <div className='container'>détail</div>;
 };
--- src/pages/PaymentReceipt/PaymentReceipt.js
+++ src/pages/PaymentReceipt/PaymentReceipt.js
@@ -1,5 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { useStoreState } from 'easy-peasy';
+
+import { useParams } from 'react-router-dom';
 
 export const PaymentReceipt = () => {
-  return <div>PaymentReceipt</div>;
+  const { id } = useParams(); // batch id
+
+  const getNowFrDate = () => {
+    const date = new Date();
+
+    const dd = date.getDate();
+    const mm = date.getMonth() + 1;
+    const yyyy = date.getFullYear();
+
+    return [dd < 10 ? '0' + dd : dd, mm < 10 ? '0' + mm : mm, yyyy].join('/');
+  };
+
+  const [paymentDate, setPaymentDate] = useState(getNowFrDate()); // todo by default get now date valid exemple value : '2020-04-06'
+  const [amount, setAmount] = useState(0);
+
+  const handleDateChange = (e) => {
+    setPaymentDate(e.target.value);
+  };
+
+  const handleSavePayment = (e) => {
+    e.preventDefault();
+
+    let date = paymentDate.split('-');
+    date = [date[2], date[1], date[0]].join('/');
+
+    console.log({ bid: id, date, amount }); // todo set this date in Firebase and in the store
+  };
+
+  return (
+    <div>
+      <h1>Réception de paiement</h1>
+
+      <form>
+        <input
+          onChange={handleDateChange}
+          type='date'
+          name='paymentDate'
+          value={paymentDate}
+        />
+
+        <input
+          onChange={(e) => setAmount(e.target.value)}
+          type='number'
+          name='amount'
+          value={amount}
+        />
+
+        <button onClick={handleSavePayment}>Sauver</button>
+      </form>
+    </div>
+  );
 };
