2e513d585b4645eba16643726d61b1edd83ad4aa
Gabriel Brun <lydstyl@gmail.com>
2020-04-11 15:58:06 +0200

    Payment date frame and toasts

--- src/pages/PaymentReceipt/PaymentReceipt.js
+++ src/pages/PaymentReceipt/PaymentReceipt.js
@@ -3,9 +3,14 @@ import { useStoreState, useStoreActions } from 'easy-peasy';
 
 import { useParams } from 'react-router-dom';
 
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 export const PaymentReceipt = () => {
   const { id } = useParams(); // batch id
-  const { batches } = useStoreState((state) => state.batch);
+  const {
+    batches,
+    utils: { dateMinus1month },
+  } = useStoreState((state) => state.batch);
   const batch = batches.filter((batch) => batch.id === id)[0];
 
   const { firestoreAddPayment, firestoreDeletePayment } = useStoreActions(
@@ -32,8 +37,37 @@ export const PaymentReceipt = () => {
   const handleSavePayment = (e) => {
     e.preventDefault();
 
+    console.log('begin', batch.beginDate);
+    let tooOldDate = dateMinus1month(batch.beginDate).split('/');
+    tooOldDate[0] = '10';
+    tooOldDate = new Date(tooOldDate[2], tooOldDate[1], tooOldDate[0]);
+
     let date = paymentDate.split('-');
+
+    const paymentDateObj = new Date(
+      date[0],
+      parseInt(date[1], 10) - 1,
+      date[2]
+    );
+
+    const now = new Date();
+
+    if (paymentDateObj < tooOldDate) {
+      M.toast({
+        html: `Vous ne pouvez pas ajouter une date si vieille`,
+      });
+      return;
+    }
+
+    if (paymentDateObj > now) {
+      M.toast({
+        html: `Vous ne pouvez pas ajouter un paiement dans le futur`,
+      });
+      return;
+    }
+
     date = [date[2], date[1], date[0]].join('/');
+
     const payment = { bid: id, date, amount };
 
     // add to payments list
