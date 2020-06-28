64cf343880a4eda709008be9a81fd8e5741ab465
Gabriel Brun <lydstyl@gmail.com>
2020-06-28 23:55:36 +0200

    red green batch

--- src/pages/Batches/Batches.jsx
+++ src/pages/Batches/Batches.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useStoreState } from 'easy-peasy';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useStoreState } from "easy-peasy";
+import { Link } from "react-router-dom";
 
 export const Batches = () => {
   const { lessors } = useStoreState((state) => state.lessor);
@@ -9,39 +9,41 @@ export const Batches = () => {
 
   return (
     <>
-      <div className='row'>
-        <h1 className='col s12'>Lots</h1>
+      <div className="row">
+        <h1 className="col s12">Lots</h1>
       </div>
 
       {!lessors.length || !tenants.length ? (
-        <p className='row'>
-          <Link className='col s12' to='/bailleurs'>
+        <p className="row">
+          <Link className="col s12" to="/bailleurs">
             Vous devez d'abord ajouter un bailleurs et un locataire pour pouvoir
             ajouter un lot
           </Link>
         </p>
       ) : (
         <>
-          <div className='row'>
-            <Link className='col s12' to='/ajouter-lot'>
+          <div className="row">
+            <Link className="col s12" to="/ajouter-lot">
               Ajouter un lot
             </Link>
           </div>
 
-          <ul className='row'>
+          <ul className="row">
             {batches.map((batch) => (
-              <li key={batch.id} className='card-content white-text col s12 m4'>
-                <div className='card blue-grey darken-1'>
-                  <div className='card-content white-text'>
-                    <span className='card-title'>
-                      {batch.name}{' '}
+              <li key={batch.id} className="card-content white-text col s12 m4">
+                <div className="card blue-grey darken-1">
+                  <div className="card-content white-text">
+                    <span className="green">ok</span>
+                    <span className="red">!!</span>
+                    <span className="card-title">
+                      {batch.name}{" "}
                       {
                         tenants.filter((tenant) => tenant.id === batch.tid)[0]
                           .lastName
                       }
                     </span>
 
-                    <ul className='card-action'>
+                    <ul className="card-action">
                       <li>
                         <Link to={`/lot/${batch.id}/avis-echeance`}>
                           Avis d'échéance
