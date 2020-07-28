a65c259fd10573b93a47ab13fb16cb97383be55e
Gabriel Brun <lydstyl@gmail.com>
2020-07-28 17:36:44 +0200

    improve batches filter feature with uppercase or lowercase

--- src/pages/Batches/Batches.jsx
+++ src/pages/Batches/Batches.jsx
@@ -1,77 +1,81 @@
-import React, { useState } from "react";
-import { useStoreState } from "easy-peasy";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
+import { useStoreState } from 'easy-peasy'
+import { Link } from 'react-router-dom'
 
 export const Batches = () => {
-  const { lessors } = useStoreState((state) => state.lessor);
-  const { tenants } = useStoreState((state) => state.tenant);
-  let { batches } = useStoreState((state) => state.batch);
+  const { lessors } = useStoreState((state) => state.lessor)
+  const { tenants } = useStoreState((state) => state.tenant)
+  let { batches } = useStoreState((state) => state.batch)
 
   batches = batches
     .map((b) => {
-      const { lastName } = tenants.filter((t) => t.id === b.tid)[0];
+      const { lastName } = tenants.filter((t) => t.id === b.tid)[0]
 
-      b.tenantLastName = lastName;
+      b.tenantLastName = lastName
 
-      return b;
+      return b
     })
     .sort((a, b) => {
       if (a.tenantLastName < b.tenantLastName) {
-        return -1;
+        return -1
       } else if (a.tenantLastName > b.tenantLastName) {
-        return 1;
+        return 1
       } else {
-        return 0;
+        return 0
       }
-    });
+    })
 
-  const [filteredBatches, setFilteredBatches] = useState(batches);
+  const [filteredBatches, setFilteredBatches] = useState(batches)
 
   const handleChange = (evt) => {
     setFilteredBatches(
-      batches.filter((b) => b.tenantLastName.startsWith(evt.target.value))
-    );
-  };
+      batches.filter((b) =>
+        b.tenantLastName
+          .toUpperCase()
+          .startsWith(evt.target.value.toUpperCase())
+      )
+    )
+  }
 
   return (
     <>
-      <div className="row">
-        <h1 className="col s12">Lots</h1>
+      <div className='row'>
+        <h1 className='col s12'>Lots</h1>
       </div>
 
       {!lessors.length || !tenants.length ? (
-        <p className="row">
-          <Link className="col s12" to="/bailleurs">
+        <p className='row'>
+          <Link className='col s12' to='/bailleurs'>
             Vous devez d'abord ajouter un bailleurs et un locataire pour pouvoir
             ajouter un lot
           </Link>
         </p>
       ) : (
         <>
-          <div className="row">
-            <Link className="col s12" to="/ajouter-lot">
+          <div className='row'>
+            <Link className='col s12' to='/ajouter-lot'>
               Ajouter un lot
             </Link>
           </div>
 
           <input
             onChange={handleChange}
-            type="text"
-            placeholder="Nom de famille du locataire"
+            type='text'
+            placeholder='Nom de famille du locataire'
           />
 
-          <ul className="row">
+          <ul className='row'>
             {filteredBatches.map((batch) => (
-              <li key={batch.id} className="card-content white-text col s12 m4">
-                <div className="card blue-grey darken-1">
-                  <div className="card-content white-text">
-                    <span className="green">ok</span>
-                    <span className="red">!!</span>
-                    <span className="card-title">
+              <li key={batch.id} className='card-content white-text col s12 m4'>
+                <div className='card blue-grey darken-1'>
+                  <div className='card-content white-text'>
+                    <span className='green'>ok</span>
+                    <span className='red'>!!</span>
+                    <span className='card-title'>
                       {batch.name} {batch.tenantLastName}
                     </span>
 
-                    <ul className="card-action">
+                    <ul className='card-action'>
                       <li>
                         <Link to={`/lot/${batch.id}/avis-echeance`}>
                           Avis d'échéance
@@ -97,5 +101,5 @@ export const Batches = () => {
         </>
       )}
     </>
-  );
-};
+  )
+}
