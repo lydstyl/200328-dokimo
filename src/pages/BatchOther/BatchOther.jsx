39ebdf901958b60c50969f7658818eadfcb95cd7
Gabriel Brun <lydstyl@gmail.com>
2020-10-25 17:56:53 +0100

    BatchOther

--- src/pages/BatchOther/BatchOther.jsx
+++ src/pages/BatchOther/BatchOther.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const BatchOther = () => {
-  return <div>BatchOther</div>
+  return (
+    <div className='row other'>
+      <h1 className='col s12'>Autres</h1>
+
+      {/* <ul>
+          <li>
+            <Link to={`/lot/${batch.id}/notes`}>Notes</Link>
+          </li>
+
+          <li>
+            <Link to={`/lot/${batch.id}/revision`}>Révision de loyer</Link>
+          </li>
+        </ul> */}
+    </div>
+  )
 }
