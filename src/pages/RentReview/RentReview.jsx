839e97a259bd90b38777c34152e91161b35887d7
Gabriel Brun <lydstyl@gmail.com>
2020-10-26 18:20:17 +0100

    RentReviewProvider & ReviewMailMaker

--- src/pages/RentReview/RentReview.jsx
+++ src/pages/RentReview/RentReview.jsx
@@ -1,5 +1,64 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+// https://docs.google.com/document/d/1wwg8v1uj7NP0SIox36DTeBtLhpAFcFW1VIwS7bqDBjs/edit#
+
+import { ReviewMailMaker } from './ReviewMailMaker'
+
+export const actionTypes = {
+  SET_SENDER: 'SET_SENDER',
+}
+
+const initialState = {
+  sender: 'Gabriel Brun',
+  recipient: 'Toto Coco',
+}
+
+function reducer(state, action) {
+  const newState = { ...state }
+
+  switch (action.type) {
+    case actionTypes.SET_SENDER:
+      newState.sender = action.payload
+
+      return newState
+
+    default:
+      // throw new Error();
+      return newState
+  }
+}
+
+export const RentReviewContext = React.createContext(initialState)
+
+const RentReviewProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <RentReviewContext.Provider value={{ state, dispatch }}>
+      {children}
+    </RentReviewContext.Provider>
+  )
+}
+
+// const Field = ({ name }) => {
+//   // const value = useContext(MyContext);
+
+//   // todo useContext to get the value like this state[name]
+
+//   // todo create action type like this `SET_${name.toCupperCase()}`
+
+//   return (
+//     <div className='field'>
+//       <label>{name}</label>
+//       <input type='text' name={name} />
+//     </div>
+//   )
+// }
 
 export const RentReview = () => {
-  return <div>RentReview</div>
+  return (
+    <RentReviewProvider>
+      <ReviewMailMaker />
+    </RentReviewProvider>
+  )
 }
---
+++ src/pages/RentReview/ReviewMailMaker.jsx
@@ -0,0 +1,111 @@
+import React, { useContext } from 'react'
+
+import { actionTypes, RentReviewContext } from './RentReview'
+
+export const ReviewMailMaker = props => {
+  const rentReviewContext = useContext(RentReviewContext)
+
+  // const [state, dispatch] = useReducer(reducer, initialState)
+  const { state, dispatch } = rentReviewContext
+
+  return (
+    <div className='row ren-review'>
+      <div className='col s12 title'>
+        <div className='row'>
+          {/* <div className='col s12 title'> */}
+          <div className='col-6 col-sm-4 title'>
+            <h1> Révision de loyer</h1>
+          </div>
+        </div>
+
+        {state && (
+          <div className='row'>
+            <form className='col l3 form'>
+              <div className='field'>
+                <label>Sender</label>
+                <input
+                  value={state.sender}
+                  onChange={evt =>
+                    dispatch({
+                      type: actionTypes.SET_SENDER,
+                      payload: evt.target.value,
+                    })
+                  }
+                  type='text'
+                  name='sender'
+                />
+              </div>
+
+              {/* <Field /> */}
+            </form>
+
+            <div className='col l9 mail'>
+              <div className='sender'>
+                <p>{state.sender}</p>
+              </div>
+
+              <div className='recipient-and-date'>
+                <p>| CIVILITÉ | | LOCATAIRE |</p>
+
+                <p>à Raismes, le | DATE_COURRIER |</p>
+              </div>
+
+              <div className='object'>
+                <p>Objet : Révision annuelle du loyer</p>
+              </div>
+
+              <div className='body'>
+                <p>| CIVILITÉ |, </p>
+
+                <p>
+                  Conformément aux dispositions de votre bail, la valeur de
+                  votre loyer est indexée sur l’évolution de l’| TYPE_INDICE |
+                  de l’INSEE du | TRIMESTRE | trimestre de chaque année.{' '}
+                </p>
+
+                <p>
+                  Récemment publié, cet indice s’établit désormais à | NI |.
+                </p>
+
+                <p>La formule de calcul de votre loyer est la suivante :</p>
+
+                <p>
+                  Nouveau loyer hors charges=Loyer hors charges Nouvel
+                  indiceAncien indice
+                </p>
+
+                <p>
+                  En conséquence, le montant de votre nouveau loyer hors charges
+                  indexé est de | NLHC | € :
+                </p>
+
+                <p>| NLHC |=| LHC | | NI | | AI |</p>
+
+                <p>
+                  En ajoutant vos charges actuelles (| CHARGES | €) nous
+                  obtenons votre nouveau loyer charges comprises: |
+                  NOUVEAU_LOYER | €.
+                </p>
+
+                <p>
+                  Cette augmentation prend effet dès à présent. Je vous remercie
+                  de bien vouloir l’appliquer lors du règlement de votre
+                  prochain loyer.{' '}
+                </p>
+
+                <p>
+                  Je vous prie de bien vouloir agréer, | CIVILITÉ |,
+                  l’expression de mes sentiments cordiaux.
+                </p>
+              </div>
+
+              <div className='signature'>
+                <p>| SIGNATURE |</p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
